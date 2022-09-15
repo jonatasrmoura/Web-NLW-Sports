0eb445974cce85166a6e272e171240700634e4ad
Jonatas Rosa Moura <jonatas.rmoura@outlook.com>
2022-09-15 20:53:48 -0300

    fix: arquitetura de pastas

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        galaxy: "url('/background-galaxy@3x.png')",
+        galaxy: "url('/background-galaxy.png')",
         // galaxy: "url('/background-galaxy.png')",
         'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
         'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 97.08%)'
